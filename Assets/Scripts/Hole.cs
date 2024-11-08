751beb25ba0d8925436b2d14b4ac5b04c1294775
ethanhimes66 <ehimes3000@gmail.com>
2024-11-08 16:51:29 -0500

    Test new golf club physics

---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    public TextMeshProUGUI scoreBoard;
+    public TextMeshProUGUI finalScoreBoard;
+    public GameObject puttArea;
+    public int holeNum;
+    public int holePar;
+    public int holeScore;
+
+    void Start()
+    {
+        //Initiate score and scoreboard text
+        holeScore = 0;
+        scoreBoard.text = holePar + "\n" + holeScore;
+        // finalScoreBoard.text = holeNum + "\n" + holePar + "\n" + holeScore;
+    }
+
+    //Update score and scoreboard text
+    public void UpdateScore(int num)
+    {
+        holeScore = num;
+        scoreBoard.text = holePar + "\n" + holeScore;
+        // finalScoreBoard.text = holeNum + "\n" + holePar + "\n" + holeScore;
+    }
+
+    //Returns the starting position of the ball
+    public Vector3 GetBallPos() {
+        return puttArea.transform.position;
+    }
+}
